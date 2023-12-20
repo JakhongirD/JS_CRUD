7ec2e2e3127b411de406a1f0bded47f5e490a615
JakhongirD <abuhafizulloh@gmail.com>
2023-12-20 10:06:40 +0500

    CRUD hometasks

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// let arr = [4, 10, 888, 99, 100];
+
+// let minValue = Math.min(...arr);
+
+// let maxValue = Math.max(...arr);
+
+// console.log(minValue);
+// console.log(maxValue);
+
+// let arr = ['olma', 'nok', 'banan', 'olma', 'nok', 'tarvuz'];
+
+// function deleteMeva(name) {
+//     arr = arr.filter(meva => meva !== name);
+// }
+
+// deleteMeva('tarvuz');
+
+// console.log(arr); 
